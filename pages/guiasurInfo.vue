96b4b16d596f2e87dd30f17faa2d178722053a3f
mjesusoj <mjesusoj@proton.me>
2023-08-24 20:40:11 +0200

    Upload pages, logos and resolve node's version :rocket:

---
+++ pages/guiasurInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="sobre-la-empresa" class="col-end-6 col-span-4 columns-xl mt-5">
+    <MazCard :images="['logo-guiasur.jpg']" orientation="square">
+      <template #title>
+        <h2 style="margin: 0; margin-bottom: 20px">
+          Sobre la empresa
+        </h2>
+      </template>
+      <template #content>
+        <p class="maz-text-muted" style="margin: 0">
+            Guiasur XXI es una empresa con sede en Sevilla (Parque Empresarial Torneo), creada en 2007 
+            y se dedica a la promoción inmobiliaria, entre otras cosas.
+        </p>
+      </template>
+    </MazCard>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import MazCard from 'maz-ui/components/MazCard'
+
+  /* definePageMeta({
+    layout: false,
+  }); */
+</script>
---
+++ pages/inmuebles.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="grid grid-cols-6 gap-4">
+        <div id="inmuebles" class="col-end-4 col-span-2  mt-5">
+          <h3 class="text-3xl text-blue-300 font-bold bg-clip-text mb-4">Inmuebles a alquilar o vender</h3>
+          <MazCard
+            :images="['logo-guiasur.jpg']"
+            :gallery-height="200"
+            zoom
+          >
+            <template #title>
+              <h3 style="margin: 0">
+                Oficina (Alquiler / Venta)
+              </h3>
+            </template>
+            <template #subtitle>
+              <span>
+                  Calle Astronomia, 1 - TORRE 3, PLT 7, MODULO 1, Sevilla, 41015 (Torneo Parque Empresarial)
+              </span>
+            </template>
+            <template #content>
+              <p class="maz-text-muted" style="margin: 0; margin-top: 20px">
+                  Este inmueble es una oficina ubicada en el parque empresarial de Torneo en Sevilla. Tiene 100m2 
+                  y muy buena localización
+              </p>
+            </template>
+          </MazCard>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+  import MazCard from 'maz-ui/components/MazCard'
+
+  /* definePageMeta({
+    layout: false,
+  }); */
+</script>
